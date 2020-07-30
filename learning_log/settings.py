29d40ada32aa3ff68397c87134e3c72bfaa5b206
Hazuki Miyake <43973945+hzkmyk@users.noreply.github.com>
2020-07-29 23:54:55 -0700

    Update settings.py

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%we_7^peiw^jq4n=yhr1t#71%(6uyeemz45p!g#0m+2ds-o(j('
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
